a671a3a563981c02a30109bfa17ae0efc8a0f07d
Lionel Joseph <joseph.lionel73@gmail.com>
2024-04-21 21:01:31 +1000

    Final Project

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Record # .models mean from the models.py file and Record is the class
 # Register your models here.
+
+admin.site.register(Record)
